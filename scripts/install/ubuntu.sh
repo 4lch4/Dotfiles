ec5cd47628f818e08582b7eedb75902ef1a9e1d5
Devin W. Leaman <git@4lch4.email>
2023-07-18 19:22:59 -0500

    refactor: moved install scripts to own dir

---
+++ scripts/install/ubuntu.sh
@@ -0,0 +1,21 @@
+################################################################################
+## Author:      Devin W. Leaman (4lch4)                                       ##
+## Version:     1.0.0                                                         ##
+## Filename:    linux.sh                                                      ##
+## Created On:  07/18/2023 @ 17:46                                            ##
+################################################################################
+## Description:                                                               ##
+##                                                                            ##
+##                                                                            ##
+## This script will install the various software/tools I use and apply my     ##
+## various customizations.                                                    ##
+################################################################################
+## Usage:                                                                     ##
+##                                                                            ##
+## ./linux.sh                                                                 ##
+################################################################################
+
+USERNAME=alcha
+
+# A URL to my `.zsrhc` file in the dotfiles repo.
+ZSHRC_URL=""
